f54aea24266cbff69a97ef11de2a66180cb7e86a
Ryan Iguchi <ryan.iguchi1@gmail.com>
2023-09-19 10:52:14 +0200

    KAN-14 Created dev branch

--- scripts/health_check.sh
+++
@@ -1,9 +0,0 @@
-response=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:4000)
-
-if [ "$response" = "200" ]; then
-    echo "Website is up and running."
-    exit 0  # Success
-else
-    echo "Website is not responding as expected (HTTP $response)"
-    exit 1  # Failure
-fi
